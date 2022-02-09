60d517b47246508b59503c6438870c9fdbe5b7be
Xayato_kakashi_ignel <mushfiq1420@gmail.com>
2022-02-09 11:11:46 +0600

    php

---
+++ MID_LAB-TASK/MID_LAB_TASK_04_PHP/task1.php
@@ -0,0 +1,10 @@
+<?php
+
+    $le= 5;
+    $wi = 6;
+
+    $area = $le*$wi;
+    $perimeter = 2*($le + $wi);
+    echo "The area of the rectangle =". $le. " and ".$wi. " is ".$area. ".........";
+    echo "The Perimeter=". $le. " and ".$wi. " is ".$perimeter;
+?>
